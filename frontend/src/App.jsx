5b4097f195ab89ec05f472f2e08bfa0a5104d9f1
sudharsan04exe <sudharsan@sudharsan>
2025-06-29 19:12:26 +0530

    broken build correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import {Route, Routes} from "react-router-dom"
 import Homepage from "./pages/Homepage"
 import Createpage from "./pages/CreatePage"
-import Notedatailpage from "./pages/Notedetailpage"
+import NoteDetailPage from "./pages/NoteDetailPage";
 import toast from 'react-hot-toast'
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element ={<Homepage/>}/>
          <Route path="/create" element ={<Createpage/>}/>
-          <Route path="/note/:id" element ={<Notedatailpage/>}/>
+          <Route path="/note/:id" element ={<NoteDetailPage/>}/>
       </Routes>
     </div>
   )
