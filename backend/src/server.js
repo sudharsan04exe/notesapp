315e1ba6a748dc4b56d3ea2a5fcb1d6c90e41e8c
sudharsan04exe <sudharsan@sudharsan>
2025-06-29 19:00:33 +0530

    next commit
    ;

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,14 @@ import notesRoutes from "./routes/notesRoutes.js"
 import { connectdb } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js"
 import cors from 'cors'
+import path from 'path'
 const app=express();
-
-app.use(cors(
+const __dirname=path.resolve()
+if(process.env.NODE_ENV!=="production"){
+    app.use(cors(
     {origin:"http://localhost:5173",}
 ));
+}
 app.use(express.json());
 app.use((req,res,next)=>{
     console.log(`req method is ${req.method} `);
@@ -17,7 +20,13 @@ app.use((req,res,next)=>{
 app.use("/api/notes", notesRoutes);
 
 
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
+app.get(/.*/,(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 connectdb().then(()=>{
 app.listen(5000,()=>{
     console.log("server started @ http://localhost:5000");
